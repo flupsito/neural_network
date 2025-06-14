e03c24314967cb7fe42e7c0be685705a4e3cd93e
florian-wulf <florian.wulf@campus.tu-berlin.de>
2025-06-14 12:58:55 +0200

    initial commit with running neural_network file

---
+++ neural_network.py
@@ -0,0 +1,110 @@
+import numpy as np
+
+class neural_network:
+	def __init__(self, hidden_layers, output_layer):
+		self.hidden_layers = hidden_layers
+		self.output_layer = output_layer
+
+	def forward(self, input):
+		for hidden_layer in self.hidden_layers:
+			hidden_layer.forward(input)
+			input = hidden_layer.output
+		self.output_layer.forward(input)
+		return self.output_layer.prediction
+
+	def backpropagation(self, y_true):
+		output = self.output_layer.backward_path(y_true)
+		for layer in reversed(self.hidden_layers):
+			output = layer.backward_path(output)
+
+	def predict(self, input):
+		return mayority_vote(self.forward(input))
+
+class dense_layer:
+	def __init__(self, n_inputs, n_outputs, learning_rate, weights = None):
+		if weights:
+			self.weights = weights
+		else:
+			self.weights = np.full(shape=(n_outputs, n_inputs), fill_value=0.1)
+		self.bias = np.zeros(shape=(n_outputs,1))
+		self.learning_rate = learning_rate
+		
+	def forward(self, input):
+		self.__pre_activation_output(input)
+
+	def __pre_activation_output(self, input):
+		self.input = input
+		self.output = np.sum(np.dot(self.weights, input) + self.bias, axis=0)
+	
+	def backward_path(self, delta):
+		delta = np.array(delta)
+		self.update_weights(delta)
+		self.update_bias(delta)
+		self.dCda = np.dot(np.transpose(self.weights), delta)
+		return self.dCda
+	
+	def update_weights(self, delta):
+		gow = self.gradient_of_weights(delta)
+		self.weights = self.weights - self.learning_rate * gow
+
+	def update_bias(self, delta):
+		self.bias = self.bias - self.learning_rate * delta
+
+	def gradient_of_weights(self, delta):
+		d = np.expand_dims(delta, axis=1)
+		i = np.expand_dims(self.input, axis=1)
+		return d * np.transpose(i)
+
+class activationLayer:
+	def __init__(self):
+		self.activate = relu
+		self.dtActivate = dtrelu
+
+	def forward(self, input):
+		self.z = input
+		self.__output()
+		return self.output
+	
+	def __output(self):
+		activate_arr = np.vectorize(self.activate)
+		self.output = activate_arr(self.z)
+
+	def backward_path(self, dCda):
+		self.delta = np.multiply(dCda, self.dtActivate(self.z))
+		return self.delta
+
+class softmax_layer:
+	def __init__(self):
+		self.prediction: np.ndarray
+		self.delta: np.ndarray
+
+	def forward(self, input):
+		self.prediction = np.zeros(shape=input.shape)
+		input = input - np.max(input)
+		sum = np.sum(np.e ** input)
+		self.prediction = np.e**input / sum
+
+	def backward_path(self, y_true):
+		self.__soft_max_error(y_true)
+		return self.delta
+
+	def __soft_max_error(self, y_true):
+		self.delta = self.prediction - y_true
+
+
+
+
+def relu(value):
+	return value if value > 0 else 0
+
+def dtrelu(z):
+	dt = [1 if value > 0 else 0 for value in z]
+	return dt
+
+def cross_entropy(prediction, true_label):
+	true_idx = np.argmax(true_label)
+	q = prediction[true_idx]
+	return -np.log(q)
+
+def mayority_vote(prediction):
+	return [1 if p == max(prediction) else 0 for p in prediction]
